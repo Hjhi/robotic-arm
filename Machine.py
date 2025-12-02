c745b0155ff2504ba942488625b6f4d7f395b7d4
Ariel Li <arielliacorn@gmail.com>
2025-12-02 15:24:38 -0800

    started new project yay

--- Machine.py
+++ Machine.py
@@ -6,8 +6,21 @@
 # - USB/serial-based controllers:
 #     import serial
 # - If you're using the DPi modules, uncomment or replace below:
-#     from dpeaDPi.DPiComputer import DPiComputer
-#     from dpeaDPi.DPiStepper import DPiStepper
+from dpeaDPi.DPiComputer import DPiComputer
+from dpeaDPi.DPiStepper import *
+from time import sleep
+
+dpiComputer = DPiComputer()
+dpiStepper = DPiStepper()
+
+#labelling sensors to make life easier
+low_pos = dpiComputer.IN_CONNECTOR__IN_0
+high_pos = dpiComputer.IN_CONNECTOR__IN_1
+
+#for servos, 90 is off, 180 is on
+magnet_servo = 0
+piston_servo = 1
+
 #
 # TODO: Replace the placeholders above with your project's actual
 # imports. Then, in Machine.__init__, initialize the appropriate
@@ -19,12 +32,43 @@
 class Machine:
     def __init__(self, **kwargs):
         #TODO: Initialize your hardware interfaces here
-        pass
+        dpiStepper.setBoardNumber(0)
 
     def halt(self):
         #TODO: Implement halt functionality
-        pass
+        dpiStepper.enableMotors(False)
+        dpiComputer.writeServo(1, 0)
+        dpiComputer.writeServo(0, 90)
+        dpiStepper.moveToAbsolutePositionInRevolutions(0, 0, True)
+
+    def stepper_startup(self):
+        dpiStepper.enableMotors(True)
+        microstepping = 8
+        dpiStepper.setMicrostepping(microstepping)
+        speed_steps_per_second = 400 * microstepping
+        accel_steps_per_second_per_second = speed_steps_per_second
+        dpiStepper.setSpeedInStepsPerSecond(0, speed_steps_per_second)
+        dpiStepper.setAccelerationInStepsPerSecondPerSecond(0, accel_steps_per_second_per_second)
+        stepperStatus = dpiStepper.getStepperStatus(0)
+        print(f"Pos = {stepperStatus}")
+
+        dpiStepper.moveToHomeInSteps(0, 1, 1600,
+                                     32000)
+        dpiStepper.setCurrentPositionInSteps(0, 0)
+        dpiStepper.setSpeedInRevolutionsPerSecond(0, 3)
+
+    def auto_move(self):
+        if dpiComputer.readDigitalIn(low_pos):
+            pass
+        elif dpiComputer.readDigitalIn(high_pos):
+            pass
+
+    def manual_move(self):
+
+
 
     def startup(self):
         #TODO: Implement startup functionality
-        pass
+        self.stepper_startup()
+        dpiComputer.writeServo(1, 0)
+        dpiComputer.writeServo(0, 90)
